d6b43de8eb485f8052c2ae17ea233483c9f1af79
tsivickas <tsivickas@gmail.com>
2021-01-29 11:42:43 +0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -3,13 +3,31 @@ namespace epgcore;
 
 class utils
 {
+
+    
     public static function max_file_upload_in_bytes() {
+        
+        $return_bytes = function return_bytes($val) {
+            $val = trim($val);
+            $last = strtolower($val[strlen($val)-1]);
+            switch($last) 
+            {
+                case 'g':
+                $val *= 1024;
+                case 'm':
+                $val *= 1024;
+                case 'k':
+                $val *= 1024;
+            }
+            return $val;
+        };
+        
         //select maximum upload size
-        $max_upload = return_bytes(ini_get('upload_max_filesize'));
+        $max_upload = $return_bytes(ini_get('upload_max_filesize'));
         //select post limit
-        $max_post = return_bytes(ini_get('post_max_size'));
+        $max_post = $return_bytes(ini_get('post_max_size'));
         //select memory limit
-        $memory_limit = return_bytes(ini_get('memory_limit'));
+        $memory_limit = $return_bytes(ini_get('memory_limit'));
         // return the smallest of them, this defines the real limit
         return min($max_upload, $max_post, $memory_limit);
     }
